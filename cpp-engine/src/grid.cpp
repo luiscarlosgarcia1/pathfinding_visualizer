8427dbed9c06298dd4c63f8cd876c597a31ac5d0
Luis Garcia <luigarcia1013@gmail.com>
2026-02-19 12:25:08 -0600

    Exposed grid getters through grid header

--- cpp-engine/src/grid.cpp
+++ cpp-engine/src/grid.cpp
@@ -1,58 +1,15 @@
 
-#pragma once
-#include "config/grid_size_reader.hpp"
-#include <vector>
+#include "grid.hpp"
 
-// struct Size { int width, height; };
-// struct Pos { int row, col; };
-
-enum class State {
-    Empty,
-    Wall,
-    Start,
-    End,
-    Visited,
-    Path,
-    Line
-};
-
-//     constexpr State empty   {0, 0, 0};
-//     constexpr State wall    {200, 200, 200};
-//     constexpr State start   {0, 255, 0};
-//     constexpr State end     {255, 0, 0};
-//     constexpr State visited {100, 150, 255};
-//     constexpr State path    {255, 165, 0};
-//     constexpr State line    {50, 50, 50};
-
-class grid
+grid::grid()
 {
-private:
-    int grid_size;
+    gridDims = config::read_grid_size("configs/config.json");
+    gridSize = gridDims * gridDims;
 
-    class cell
+    cells.reserve(gridSize);
+
+    for (int i = 0; i < gridSize; i++)
     {
-    public:
-        State state;
-        cell (State st) 
-        {
-            state = st;
-        }
-    };
-    std::vector<cell> cells;
-
-
-public:
-    grid() 
-    {
-        grid_size = config::read_grid_size("configs/config.json");
-
-        cells.reserve(grid_size*grid_size);
-
-        for (int i = 0; i < grid_size*grid_size; i++)
-        {
-            cells.emplace_back(State::Empty);
-        }
+        cells.emplace_back(State::Empty);
     }
-    ~grid() = default;
-    
-};
+}
---
+++ cpp-engine/src/grid.hpp
@@ -0,0 +1,51 @@
+
+#pragma once
+#include "config/grid_size_reader.hpp"
+#include <vector>
+using namespace std;
+
+enum class State {
+    Empty,
+    Wall,
+    Start,
+    End,
+    Visited,
+    Path,
+    Line
+};
+
+// {0, 0, 0};
+// {200, 200, 200};
+// {0, 255, 0};
+// {255, 0, 0};
+// {100, 150, 255};
+// {255, 165, 0};
+// {50, 50, 50};
+
+class grid
+{
+private:
+    int gridDims;
+    int gridSize;
+
+    class cell
+    {
+    public:
+        State state;
+        cell (State st) 
+        {
+            state = st;
+        }
+    };
+
+    vector<cell> cells;
+
+
+public:
+    grid();
+    ~grid() = default;
+
+    int getGridDims() { return gridDims; }
+    int getGridSize() { return gridSize; }
+    const vector<cell>& getCells() { return cells; }
+};
